522d3bd66c44ba0382b11f8e3219203dc858c80e
Adonaiis <yoann.frac@gmail.com>
2021-03-24 15:34:17 +0100

    OC Projet 4 - Update 2.4
    HTML:
    - Ajout d'un texte plus complet afin de remplacer la courte phrase de présentation des sites réalisés par LCA
    - Mise à jour de la page de contact
    
    CSS:
    - Modifications et adaptations de plusieurs éléments pour la mise en forme des pages index et contact
    - Ajout d'une class "coordonnees"
    - Retrait d'une ligne dans le font-face
    
    IMAGES:
    - Réduction de la dimension du logo afin qu'il s'adapte parfaitement aux valeurs attendus
    
    PHP:
    - Retrait de certains underscore présent. Test !!!

--- includes/form_1.php
+++ includes/form_1.php
@@ -1,12 +1,12 @@
 <?php	
-	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['input_504']) && strlen($_POST['input_504']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
+	if(empty($_POST['name']) && strlen($_POST['name']) == 0 || empty($_POST['email']) && strlen($_POST['email']) == 0 || empty($_POST['input-504']) && strlen($_POST['input-504']) == 0 || empty($_POST['message']) && strlen($_POST['message']) == 0)
 	{
 		return false;
 	}
 	
 	$name = $_POST['name'];
 	$email = $_POST['email'];
-	$input_504 = $_POST['input_504'];
+	$input_504 = $_POST['input-504'];
 	$message = $_POST['message'];
 	
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
@@ -14,7 +14,7 @@
 	// Create email	
 	$email_subject = "Message from your website";
 	$email_body = "You have received a new message. \n\n".
-				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
+				  "Name: $name \nEmail: $email \nInput-504: $input_504 \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\n";
 	$headers .= "Reply-To: $input_504";	
